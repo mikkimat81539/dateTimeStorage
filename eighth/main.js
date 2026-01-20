03ac2afd97a9d1e47ba0caf59e51cc0bf6eb0de9
Mikaela Matthews <mikaelamatthews@Mikaelas-MacBook-Air.local>
2026-01-20 10:14:24 -0600

    defined dow attribute

--- eighth/main.js
+++ eighth/main.js
@@ -1,7 +1,20 @@
 const submitBtn = document.getElementById('submitBtn')
+const clearBtn = document.getElementById('clearBtn')
+const dayData = document.querySelectorAll('tr[data-day]')
+
+function DOWstorage(){
+	for (let i=0; i < dayData.length; i++){
+		const days = dayData[i].getAttribute('data-day')
+		console.log(days)
+	}
+}
 
 
 submitBtn.addEventListener('click', (ev) => {
 	ev.preventDefault()
+	DOWstorage()
+})
 
+clearBtn.addEventListener('click', () => {
+	console.clear()
 })
