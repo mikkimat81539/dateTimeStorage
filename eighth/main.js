93b92cf95886076f7db2a0a30758646b88069263
Mikaela Matthews <mikaelamatthews@Mikaelas-MacBook-Air.local>
2026-01-20 10:48:45 -0600

    added time to console

--- eighth/main.js
+++ eighth/main.js
@@ -4,8 +4,19 @@ const dayData = document.querySelectorAll('tr[data-day]')
 
 function DOWstorage(){
 	for (let i=0; i < dayData.length; i++){
-		const days = dayData[i].getAttribute('data-day')
-		console.log(days)
+		const days = dayData[i].getAttribute('data-day') // key
+		const timeInputs = document.querySelectorAll('input')
+		
+		for (let i=0; i < timeInputs.length; i++){
+			if (!timeInputs[i].value){
+				console.log('nothing')
+			}
+		
+			else {
+				console.log(timeInputs[i].value , days)
+			}
+		}
+
 	}
 }
 
