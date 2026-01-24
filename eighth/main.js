38ef7e169ac9c6d45d875c636dd906bd82215e9a
Mikaela Matthews <mikaelamatthews@Mikaelas-MacBook-Air.local>
2026-01-24 05:32:29 -0600

    created div and p element for error function

--- eighth/main.js
+++ eighth/main.js
@@ -1,10 +1,38 @@
 const submitBtn = document.getElementById('submitBtn')
 const clearBtn = document.getElementById('clearBtn')
 const myItems = document.querySelectorAll("tbody > tr")
-// const startTime = document.querySelectorAll('.startTime')
-// const endTime = document.querySelectorAll('.endTime')
+// let div = document.createElement('div')
+// let p = document.createElement('p')
 
 document.addEventListener("DOMContentLoaded", DOWstorage)
+
+function errorContainer(i){
+	div = document.createElement('div')
+	p = document.createElement('p')
+
+	// styling div
+	div.style.width = "250px"
+	div.style.height = "30px"
+	div.style.display = "flex"
+	div.style.justifyContent = "center"
+	div.style.alignItems = "center"
+	div.style.userSelect = "none"
+
+	// styling p 
+	p.style.color = "red"
+	p.style.margin = "0px"
+	p.style.fontFamily = "Arial, sans-serif"
+	p.style.fontSize = "15px"
+
+	myItems[i].append(div)
+	p.innerHTML = "Input both Start Time and End Time"
+	div.append(p)
+}
+
+function removeError(i){
+	div.remove()
+	p.remove()
+}
 
 function DOWstorage(){
 	for (let i=0; i < myItems.length; i++){
@@ -14,9 +42,6 @@ function DOWstorage(){
 		//console.log(myRows)	
 		
 		const myInputs = myRows.querySelectorAll("td > input")
-
-		// const startTime = myRows.querySelectorAll('.startTime')
-		// const endTime = myRows.querySelectorAll('.endTime')		
 
 		let myList = []		
 
@@ -38,16 +63,22 @@ function DOWstorage(){
 		}
 
 		else if (0 < myList.length && myList.length < myInputs.length){
-			//console.log(rowAttr, "ERROR")
 			localStorage.removeItem(rowAttr)
-			throw "ERROR"
+			// throw "ERROR"
+						
+			if (myList.length === myInputs.length){
+				removeError(i)
+			}
+			
+			else {
+				errorContainer(i)
+			}
 		}
 		
 		else {
 			localStorage.setItem(rowAttr, myList)
 			console.clear()
 		}	
-		//debugger
 	}
 }
 
@@ -57,7 +88,8 @@ submitBtn.addEventListener('click', (ev) => {
 	DOWstorage()
 })
 
-clearBtn.addEventListener('click', () => {
+clearBtn.addEventListener('click', (i) => {
 	console.clear()
 	localStorage.clear()
+	removeError(i)
 })
